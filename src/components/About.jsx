f99b5e9d6b6bbe3bb3be6fff3d7a831bd1fc6e7c
Shishu Raj <106923546+Craaaj@users.noreply.github.com>
2025-09-16 03:48:50 +0530

    add AnimatedTitle component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollToPlugin } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollToPlugin)
 
@@ -26,33 +27,31 @@ const About = () => {
     })
 
   return (
-    <div id="about"  className="min-h-screen w-screen">
-        <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-            <h2 className="font-general text-sm uppercase md:text-[10px]">
-                Welcome to Zentry
-            </h2>
-            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-            </div>
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </h2>
 
-            <div className="about-subtext">
-                <p>The Game of Games begins-your life, now an epic MMORPG</p>
-                <p>
-                    Zentry unites every player from countless games and platforms
-                </p>
-            </div>
+        <AnimatedTitle title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center"/>
+
+        <div className="about-subtext">
+          <p>The Game of Games begins-your life, now an epic MMORPG</p>
+          <p>Zentry unites every player from countless games and platforms</p>
         </div>
-            <div className="h-dvh w-screen" id="clip">
-                <div className="mask-clip-path about-image">
-                    <img
-                     src="img/about.webp"
-                     alt="Background"
-                     className="absolute left-0 top-0 size-full object-cover"
-                    />
-                </div>
-            </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default About
