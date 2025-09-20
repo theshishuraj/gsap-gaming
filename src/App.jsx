43b3bd0bf07553e2124f6f0b06de38cfadc9e8fc
Shishu Raj <106923546+Craaaj@users.noreply.github.com>
2025-09-20 23:28:45 +0530

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Features from "./components/Features"
 import Story from "./components/Story"
+import Contact from "./components/Contact"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <About/>
       <Features/>
       <Story/>
+      <Contact/>
     </main>
   )
 }
